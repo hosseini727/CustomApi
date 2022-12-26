7e190571c7c60716a3b3c80871bb267bf6605e97
hosseini727 <esmaeil7@yahoo.com>
2022-12-26 17:25:45 +0330

    ff

--- Common/Utilities/Assert.cs
+++ Common/Utilities/Assert.cs
@@ -6,7 +6,7 @@ namespace Common.Utilities
 {
     public static class Assert
     {
-        //بررسی پامترهای ورودی که نال نباشند //test33
+        //بررسی پامترهای ورودی که نال نباشند //testessi
         public static void NotNull<T>(T obj, string name, string message = null)
             where T : class
         {
